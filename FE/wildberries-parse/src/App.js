fa5619fc293eabeea8bc0c5c41e93c71de151bad
AndYuz27 <yuzlov2003@gmail.com>
2022-12-21 23:13:27 +0300

    добавлениe header'а в react

--- FE/wildberries-parse/src/App.js
+++ FE/wildberries-parse/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Link, Route, Routes} from "react-router-dom"
+import Home from './pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{/* <h1>It's works</h1> */}
+    <header>
+      <div className='logo'>
+        <h2>WB-Parse</h2>
+      </div>
+      <nav>
+
+      <Link to="/" className="hdr_link">Account</Link>
+      <Link to="/" className="hdr_link">Catalog</Link>
+      <Link to="/" className="hdr_link">Parse Settings</Link>
+      </nav>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </main>
     </div>
   );
 }
