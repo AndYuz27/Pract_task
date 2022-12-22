2d43f8400511ec49e886851717f4d868fc355bef
AndYuz27 <yuzlov2003@gmail.com>
2022-12-22 15:07:31 +0300

    Добавление муляжа карточек товаров

--- FE/wildberries-parse/src/App.js
+++ FE/wildberries-parse/src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
 import {Link, Route, Routes} from "react-router-dom"
 import Home from './pages/Home/Home';
+import Catalog from './pages/Catalog/Catalog';
 function App() {
   return (
     <div className="App">
 {/* <h1>It's works</h1> */}
     <header>
       <div className='logo'>
-        <h2>WB-Parse</h2>
+      <Link to="/" className="hdr_link"><h2>WB-Parse</h2></Link>
       </div>
       <nav>
 
       <Link to="/" className="hdr_link">Account</Link>
-      <Link to="/" className="hdr_link">Catalog</Link>
+      <Link to="/cat" className="hdr_link">Catalog</Link>
       <Link to="/" className="hdr_link">Parse Settings</Link>
       </nav>
     </header>
     <main>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/cat" element={<Catalog/>}/>
       </Routes>
     </main>
     </div>
